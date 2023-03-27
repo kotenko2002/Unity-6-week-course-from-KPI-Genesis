4dc47113590cfeb7bda59b1becfd5e324914158a
Dmytro Kotenko <dmytro.ko@qualer.com>
2023-03-27 22:42:08 +0300

    refactoring jump

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -1,63 +1,42 @@
 using Assets.Scripts.Core.Animation;
 using Assets.Scripts.Movement.Controller;
 using Assets.Scripts.Movement.Data;
-using Assets.Scripts.Player;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerEntity : MonoBehaviour
 {
     [SerializeField] private AnimatorController _animator;
-
     [SerializeField] private HorizontalMovementData _horizontalMovementData;
-
-    [Header("Jump")]
-    [SerializeField] private float _jumpForce;
-    [SerializeField] private LayerMask _groundLayer;
-
+    [SerializeField] private JumperData _jumperData;
     [SerializeField] private DirectionalCameraPair _cameras;
 
     private Rigidbody2D _rigidbody;
-    private BoxCollider2D _collider;
-    private bool _isGrounded;
-
     private HorizontalMover _horizontalMover;
+    private Jumper _jumper;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<BoxCollider2D>();
         _horizontalMover = new HorizontalMover(_rigidbody, _horizontalMovementData);
-
-        _isGrounded = _collider.IsTouchingLayers(_groundLayer);
+        _jumper = new Jumper(_rigidbody, GetComponent<BoxCollider2D>(), _jumperData);
     }
 
     private void Update()
     {
-        _isGrounded = _collider.IsTouchingLayers(_groundLayer);
-
         UpdateAnimations();
         UpdateCameras();
     }
 
     public void MoveHorizontally(float direction) => _horizontalMover.MoveHorizontally(direction);
 
-    public void Jump()
-    {
-        if (!_isGrounded)
-        {
-            return;
-        }
-
-        _isGrounded = true;
-        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
-    }
+    public void Jump() => _jumper.Jump();
     
     private void UpdateAnimations()
     {
         _animator.PlayAnimation(AnimationType.Idle, true);
         _animator.PlayAnimation(AnimationType.Run, _horizontalMover.IsMoving);
-        _animator.PlayAnimation(AnimationType.Jump, !_isGrounded);
+        _animator.PlayAnimation(AnimationType.Jump, !_jumper.IsGrounded);
     }
 
     private void UpdateCameras()
